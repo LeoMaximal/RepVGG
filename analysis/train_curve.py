27f2fbbd561272eee04b0aec0269bb221ba7a7f8
LeoMaximal <LeoMaximal@outlook.com>
2021-07-15 21:58:11 +0800

    train curve add lr

--- analysis/train_curve.py
+++ analysis/train_curve.py
@@ -5,26 +5,25 @@ plt.rcParams['font.size']=20
 epochs=52
 loss=np.zeros(epochs)
 acc=np.zeros((2,epochs))
-count=0
+lr=np.zeros(epochs*11)
+count1=0
+count2=0
 f=open('/Users/leoma/Programming/Python/RepVGG/analysis/log_A0.txt')
 for i in range(1374):
     if (i-149)%24==0 and i>=149:
         if (i-149)/24 < 10:
-            loss[count]=float(str[83:93])
-            acc[0,count]=float(str[110:115])
-            acc[1,count]=float(str[132:137])
-            print(float(str[83:93]))
-            print(float(str[110:115]))
-            print(float(str[132:137]))
+            loss[count1]=float(str[83:93])
+            acc[0,count1]=float(str[110:115])
+            acc[1,count1]=float(str[132:137])
         else:
-            loss[count]=float(str[84:94])
-            acc[0,count]=float(str[111:116])
-            acc[1,count]=float(str[133:138])
-            print(float(str[84:94]))
-            print(float(str[111:116]))
-            print(float(str[133:138]))
-        count+=1
+            loss[count1]=float(str[84:94])
+            acc[0,count1]=float(str[111:116])
+            acc[1,count1]=float(str[133:138])
+        count1+=1
     str=f.readline()
+    if str.find('cur lr') != -1 and count2 <= epochs*11:
+        lr[count2]=float(str[9:21])
+        count2+=1
 f.close()
 
 fig,ax=plt.subplots(figsize=(8,8))
@@ -38,3 +37,9 @@ ax.plot(acc[1],label='Top 5')
 ax.set(xlabel='epoch',ylabel='accuracy(%)')
 ax.legend()
 plt.savefig('acc.png')
+
+x=np.linspace(0,epochs,epochs*11)
+fig,ax=plt.subplots(figsize=(8,8))
+ax.plot(x,lr)
+ax.set(xlabel='epoch',ylabel='learning rate')
+plt.savefig('lr.png')
